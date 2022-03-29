76ce88db86c6c41ba854ba9609be4e50767d48dd
Amrit199 <44949877+Amrit199@users.noreply.github.com>
2022-03-29 18:34:54 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const videocontainer = document.querySelector('.video-container')
+const btns = document.querySelector('.switch-btn')
+
+btns.addEventListener('click', () => {
+    if(!btns.classList.contains('slide')) {
+        btns.classList.add('slide')
+        videocontainer.pause()
+    }else {
+        btns.classList.remove('slide')
+        videocontainer.play()
+    }
+})
+
+// Nav toggle
+
+const navToggle = document.querySelector('.nav-toggle')
+const navMenu = document.querySelector('.sidebar')
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show-sidebar')
+})
